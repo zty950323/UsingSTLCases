7034e38d0b55e4ddaf77f2dcc2098b0017ede778
Tom Zhao <18872770445@163.com>
2023-08-14 10:30:56 +0800

    Adjust some code.
    Signed-off-by: Tom Zhao <18872770445@163.com>

---
+++ DesignPattern/CreationalPatterns/FactoryMethod/inc/Subjects/fd_english_subject.h
@@ -0,0 +1,41 @@
+/**
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 1. You can modify and use this program at will.
+* 2. But it is suggested that it is more suitable for learning and teaching
+* purposes.
+*
+* @version 1.0
+* @brief This file provide a english subject for using.
+* @file basic_config_service.h
+* @author Tom Zhao(18872770445@163.com)
+* @since 2023-04-23
+* @date Created: 2023-04-23
+* @date Last modified: 2023-04-23
+*
+* Github: @link https://github.com/zty950323/UsingSTLCases @endlink
+* Homepage: @link https://blog.csdn.net/zhaotianyu950323 @endlink
+* Blog: @link https://www.zhihu.com/people/Tom_Zhao @endlink
+*/
+
+#ifndef _FD_ENGLISH_SUBJECT_H_H_
+#define _FD_ENGLISH_SUBJECT_H_H_
+
+namespace FactoryMethod
+{
+    namespace Subjects
+    {
+        class English final
+        {
+        public:
+            English() = default;
+            ~English() = default;
+        };
+    }  // namespace Subjects
+}  // namespace FactoryMethod
+
+#endif // _FD_ENGLISH_SUBJECT_H_H_
